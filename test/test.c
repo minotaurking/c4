c429fca8d8d82d2c759e03160220f6515b071cd6
kai <minotaurking@gmail.com>
2025-03-14 22:29:12 +0800

    feat: add some simple functions
    Add some simple functions. You can treat them as unittest. If c4 and
    gen_code were changed, run this functions to test to make sure the
    change works fine

--- test/test.c
+++ test/test.c
@@ -1,7 +1,9 @@
-int a;
-int b;
-char c;
-int *ppp;
+int failed_case;
+int ia;
+int ib;
+int ic;
+char ca;
+int *pa;
 
 void putchar(char c) {
     char *p;
@@ -80,24 +82,71 @@ void print3(char *s, int p1, int p2, int p3) {
 void assert(char *file, int line, char *expr, int cond) {
     if (cond == 0) {
         print3("assert failed!\n%s:%d\n%s\n", file, line, expr);
+        failed_case++;
     }
 }
 
-void test(int t) {
-    int *pp;
-    pp = &t;
-    if (*pp == 3) {
-        print0("3\n");
-    }
+// Test global variable access
+void test_global() {
+    ia = 10;
+    ib = 20;
+    ic = ia + ib;
+    ASSERT(ic == 30);
+}
+
+// Test local variable access
+void test_local() {
+    int a;
+    int b;
+    int c;
+    a = 10;
+    b = 20;
+    c = a + b;
+    ASSERT(c == 30);
+}
+
+// Test function parameters and return value
+int test_func_para(int a, int b) {
+    int c;
+    c = a + b;
+    return c;
+}
+
+// Pass global variable as parameters
+void test_func_global() {
+    ia = 30;
+    ib = 40;
+    ic = test_func_para(ia, ib);
+    ASSERT(ic == 70);
+}
+
+// Pass local variable as parameters
+void test_func_local() {
+    int a;
+    int b;
+    int c;
+    a = 30;
+    b = 40;
+    c = test_func_para(a, b);
+    ASSERT(c == 70);
+}
+
+void test_large_number() {
+    int a;
+    int b;
+
+    a = 65536;
+    b = a - 32768;
+    ASSERT(b == 32768);
 }
 
 int main() {
-    a = 12;
-    b = 23;
-    print4("print %d %d %s %d\n", a, b, "abc", b + 2);
+    failed_case = 0;
+    test_global();
+    test_local();
+    test_func_global();
+    test_large_number();
 
-    ASSERT(b == 23);
-    ASSERT(a == 13);
-
+    print1("failed_case: %d\n", failed_case);
     return 0;
 }
